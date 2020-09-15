79d0b732b0ad2a4f2d1cad53d2a4adc5969506dc
Anabella <ana.da.rocha@hotmail.com>
2020-09-15 13:14:38 -0300

    media queries modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter as Router} from 'react-router-dom';
 import Navbar from './components/Navbar/Navbar';
 import Header from './components/Header/Header';
 import About from './components/About/About';
+import Tech from './components/Tech/Tech';
 import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
@@ -11,16 +12,17 @@ import './App.css';
 class App extends Component {
   render(){
   return (
-    <>
+    <div className='app'>
     <Router>
     <Navbar />
     <Header />
     <About />
+    <Tech />
     <Projects />
     <Contact />
     <Footer />
     </Router>
-    </>
+    </div>
   );
   }
 }
---
+++ src/components/Tech/Tech.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Tech = () => {
+    return (
+        <div>
+            <h1>What can I do?</h1>
+            <div>
+                <img src='./js.png'/>
+            </div>
+        </div>
+    )
+};
+
+export default Tech;
+
+
