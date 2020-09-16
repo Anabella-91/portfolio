f1b34a607fc54dbe230f1a678558e7afec2fd0bb
Anabella <ana.da.rocha@hotmail.com>
2020-09-16 16:47:37 -0300

    prueba

--- src/components/Tech/Tech.js
+++ src/components/Tech/Tech.js
@@ -5,6 +5,7 @@ const Tech = () => {
     return (
         <div className='tech-container'>
             <h1>What can I do?</h1>
+            <div className='logos'>
             <img src='./html.png'/>
             <img src='./css.png'/>
             <img src='./js.png'/>
@@ -13,6 +14,7 @@ const Tech = () => {
             <img src='./mysql.png'/>
             <img src='./mongodb.png'/>
             <img src='./git.png'/>
+            </div>
         </div>
     )
 };
